1f4468b1aa8e7949a7b5727b2d7073f34343df1e
ogurim <80939431+kimjeongrim@users.noreply.github.com>
2024-07-19 14:58:51 +0900

    Add files via upload

---
+++ FourU3/02_terraform/01_data.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "foryou"
+  cidr = var.vpc_cidr
+
+  azs             = var.availability_zones
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+  database_subnets = var.db_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  create_igw = true
+  map_public_ip_on_launch = true
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  public_subnet_enable_resource_name_dns_a_record_on_launch = true
+
+  tags = {
+    Terraform = "true"
+  }
+}
