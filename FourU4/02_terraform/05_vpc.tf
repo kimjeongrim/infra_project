25577cfb9c0ea85e617fb8c2ad341e55fcfda5cd
ogurim <80939431+kimjeongrim@users.noreply.github.com>
2024-07-20 12:44:59 +0900

    Delete FourU4 directory

--- FourU4/02_terraform/05_vpc.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_route_table_association" "igw_rtas" {
-  count          = length(module.vpc.public_subnets)
-  subnet_id      = element(module.vpc.public_subnets, count.index)
-  route_table_id = element(module.vpc.public_route_table_ids, 0)
-}
